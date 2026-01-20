79da3a347af8c5e104395e9f4236604e3269d468
Yanis Kouidri <yanis.kouidri@gmail.com>
2026-01-20 19:09:02 +0100

    refactor: add constant

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import hashlib
 import hmac
 import secrets
+from typing import Literal
 
 import bech32
 import bip39
 import ecdsa
 
+BYTE_ORDER: Literal["big", "little"] = "big"
+HMAC_DIGEST_ALGO = "sha512"
+# 0 = Mainnet, 1 = Testnet
+COIN_TYPE = 1
+
+# Human Readable Part : bc = mainnet, tb = testnet
+HRP_PREFIX = "tb"
+
 
 def compute_compressed_pub_key(private_key: bytes) -> bytes:
-    public_key = int.from_bytes(private_key, "big") * ecdsa.SECP256k1.generator
+    public_key = int.from_bytes(private_key, BYTE_ORDER) * ecdsa.SECP256k1.generator
     x = public_key.x()
     prefix = 0x02 if public_key.y() % 2 == 0 else 0x03
-    compressed_pub_key = int.to_bytes(prefix, 1, "big") + int.to_bytes(x, 32, "big")
+    compressed_pub_key = int.to_bytes(prefix, 1, BYTE_ORDER) + int.to_bytes(x, 32, BYTE_ORDER)
     return compressed_pub_key
 
 
 def compute_child_keys(parent_private_key: bytes, parent_chain_code: bytes, child_index: int, is_hardened: bool) -> \
-tuple[bytes, bytes]:
+        tuple[bytes, bytes]:
     if is_hardened:
-        control_byte = int.to_bytes(0, 1, byteorder="big")
-        hardened_index = int.to_bytes(2 ** 31 + child_index, 4, byteorder="big")
+        control_byte = int.to_bytes(0, 1, byteorder=BYTE_ORDER)
+        hardened_index = int.to_bytes(2 ** 31 + child_index, 4, byteorder=BYTE_ORDER)
         message = control_byte + parent_private_key + hardened_index
 
     else:
         compressed_pub_key = compute_compressed_pub_key(parent_private_key)
-        message = compressed_pub_key + int.to_bytes(child_index, 4, "big")
+        message = compressed_pub_key + int.to_bytes(child_index, 4, BYTE_ORDER)
 
-    product_hmac = hmac.digest(key=parent_chain_code, msg=message, digest="sha512")
-    child_private_key = ((int.from_bytes(product_hmac[0:32], "big") + int.from_bytes(parent_private_key, "big"))
-                         % ecdsa.SECP256k1.order).to_bytes(32, "big")
+    product_hmac = hmac.digest(key=parent_chain_code, msg=message, digest=HMAC_DIGEST_ALGO)
+    child_private_key = (
+            (int.from_bytes(product_hmac[0:32], BYTE_ORDER) + int.from_bytes(parent_private_key, BYTE_ORDER))
+            % ecdsa.SECP256k1.order).to_bytes(32, BYTE_ORDER)
     child_chain_code = product_hmac[32:64]
     return child_private_key, child_chain_code
 
@@ -38,15 +48,15 @@ def generate_binary_seed() -> bytes:
 
     # print(entropy)
 
-    bip39_phrase = bip39.encode_bytes(entropy.to_bytes(32, "big"))
+    bip39_phrase = bip39.encode_bytes(entropy.to_bytes(32, BYTE_ORDER))
     print(bip39_phrase)
 
-    binary_seed = hashlib.pbkdf2_hmac("sha512", bip39_phrase.encode(), b"mnemonic", 2048)
+    binary_seed = hashlib.pbkdf2_hmac(HMAC_DIGEST_ALGO, bip39_phrase.encode(), b"mnemonic", 2048)
     return binary_seed
 
 
 def compute_master_keys(binary_seed: bytes) -> tuple[bytes, bytes]:
-    master = hmac.digest(key=b"Bitcoin seed", msg=binary_seed, digest="sha512")
+    master = hmac.digest(key=b"Bitcoin seed", msg=binary_seed, digest=HMAC_DIGEST_ALGO)
     master_private_key = master[0:32]
     master_chain_code = master[32:64]
     return master_private_key, master_chain_code
@@ -61,7 +71,7 @@ def compute_seg_wit_native_bitcoin_address(level4_private_key: bytes) -> str:
     # print(len(hash160_pub_key))
 
     data_5bit = bech32.convertbits(hash160_pub_key, 8, 5)
-    final_pub_key = bech32.encode("bc", 0, list(data_5bit))
+    final_pub_key = bech32.encode(HRP_PREFIX, 0, list(data_5bit))
     return final_pub_key
 
 
@@ -72,7 +82,7 @@ def main():
     level0_private_key, level0_chain_code = compute_child_keys(master_private_key, master_chain_code, 84, True)
 
     # Level 1 Coin Type
-    level1_private_key, level1_chain_code = compute_child_keys(level0_private_key, level0_chain_code, 0, True)
+    level1_private_key, level1_chain_code = compute_child_keys(level0_private_key, level0_chain_code, COIN_TYPE, True)
 
     # Level 2 Account
     level2_private_key, level2_chain_code = compute_child_keys(level1_private_key, level1_chain_code, 0, True)
