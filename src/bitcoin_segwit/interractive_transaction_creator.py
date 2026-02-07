6c14068bfe9db4a6a11fced1af38051f7feb1a1f
Yanis Kouidri <yanis.kouidri@gmail.com>
2026-02-07 09:46:06 +0100

    feat: propose to send tx on the network

--- src/bitcoin_segwit/interractive_transaction_creator.py
+++ src/bitcoin_segwit/interractive_transaction_creator.py
@@ -1,6 +1,9 @@
+import httpx
 import questionary
 
 from bitcoin_segwit.models import NativeSegWitBitcoinTransaction
+
+TEST_NET4_URL = "https://mempool.space/testnet4/api/tx"
 
 
 def main():
@@ -8,19 +11,19 @@ def main():
 
     while True:
         bitcoin_input_form = questionary.form(
-            in_txid=questionary.text("What is your first txid ?"),
+            in_txid=questionary.text("What is the txid ?"),
             in_vout=questionary.text(
                 "Which vout of this txid use ?",
                 validate=lambda text: True
                 if text.isdigit()
                 else "Please provide an int",
             ),
-            in_bitcoin_address=questionary.text("What is the source bitcoin address ?"),
             in_value=questionary.text("How many satoshis this output has ?"),
+            in_bitcoin_address=questionary.text("What is the source bitcoin address ?"),
+            in_public_key=questionary.text("What is the public key of this input ?"),
             in_private_key=questionary.password(
                 "What is the private key of this input ?"
             ),
-            in_public_key=questionary.text("What is the public key of this input ?"),
         )
 
         bitcoin_input_answer = bitcoin_input_form.ask()
@@ -33,9 +36,16 @@ def main():
             bitcoin_input_answer["in_public_key"],
         )
 
-        is_finish = not questionary.confirm("Do you want to add another input ?").ask()
+        is_finish = not questionary.confirm(
+            message="Do you want to add another input ?", default=False
+        ).ask()
         if is_finish:
             break
+
+    satoshis_to_spend = 0
+    for tx_input in transaction.inputs:
+        satoshis_to_spend += tx_input.value
+    print(f"You have {satoshis_to_spend} satoshis to spend")
 
     while True:
         bitcoin_output_form = questionary.form(
@@ -50,11 +60,24 @@ def main():
             int(bitcoin_output_answer["out_amount"]),
             bitcoin_output_answer["out_address"],
         )
-        is_finish = not questionary.confirm("Do you want to add another output ?").ask()
+        satoshis_to_spend -= int(bitcoin_output_answer["out_amount"])
+
+        is_finish = not questionary.confirm(
+            message="Do you want to add another output ?", default=False
+        ).ask()
         if is_finish:
+            print(f"Fees for this transaction will be: {satoshis_to_spend} satoshis")
             break
+        print(
+            f"You still have {satoshis_to_spend} satoshis to spend, the rest will be used as fees"
+        )
 
     print(f"Transaction: {transaction.serialization()}")
+    send_tx = questionary.confirm(
+        message="Do you want to send the transaction to the testnet4 ?", default=True
+    ).ask()
+    if send_tx:
+        httpx.post(url=TEST_NET4_URL, content=transaction.serialization())
 
 
 if __name__ == "__main__":
