fe446b5c2f5d8aa93d0435c2d5116a5cf4169029
Yanis Kouidri <yanis.kouidri@gmail.com>
2026-02-07 09:08:06 +0100

    fix: use BIP 62 rule for signature

--- src/bitcoin_segwit/crypto_utils.py
+++ src/bitcoin_segwit/crypto_utils.py
@@ -5,13 +5,19 @@ import ecdsa.rfc6979
 from ecdsa import SECP256k1
 
 
+def signcode_bitcoin_der(r, s, order):
+    if s > order // 2:
+        s = order - s
+    return ecdsa.util.sigencode_der(r, s, order)
+
+
 def sign_preimage_hash(sighash_preimage: bytes, private_key: int) -> bytes:
     sk = ecdsa.SigningKey.from_secret_exponent(
         secexp=private_key, curve=SECP256k1, hashfunc=hashlib.sha256
     )
 
     signature_preimage = sk.sign_digest_deterministic(
-        digest=sighash_preimage, sigencode=ecdsa.util.sigencode_der
+        digest=sighash_preimage, sigencode=signcode_bitcoin_der
     )
     sighash_flag = bytes([1])
     signature_preimage += sighash_flag
